0b870b116a52126aa67c052484aaed0cd5f09993
SolomonOnwueyikpe <solofamily2003@yahoo.com>
2024-07-07 16:53:05 -0700

    NoSQL

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -19,3 +19,27 @@ if __name__ == "__main__":
     status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
+
+    print(f'{status_check} status check')
+
+    top_ips = nginx_collection.aggregate([
+        {"$group":
+            {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
+         },
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
+    ])
+
+    print("IPs:")
+    for top_ip in top_ips:
+        ip = top_ip.get("ip")
+        count = top_ip.get("count")
+        print(f'\t{ip}: {count}')
